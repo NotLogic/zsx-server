e8f3d7d334f81485c2a468b0606aafdc12fc893d
AndreaChow <137842014@qq.com>
2018-01-24 20:59:51 +0800

    页面内容添加

--- src/pages/ad/index.vue
+++ src/pages/ad/index.vue
@@ -1,13 +1,501 @@
 <template>
   <div class="ad">
-    广告管理
-    <router-link :to="{name: 'main'}">返回主页</router-link>
+    <Form inline>
+      <FormItem>
+        <Button type="primary" @click="addRow" size="small">添加</Button>
+      </FormItem>
+    </Form>
+    <!-- 高级搜索 -->
+    <Form :model="formSearch" ref="formSearch" inline :label-width="60">
+      <FormItem label="关键字" prop="title">
+        <Input v-model="formSearch.title" placeholder="标题" size="small"></Input>
+      </FormItem>
+      <FormItem label="广告位置" prop="postion">
+        <Select v-model="formSearch.postion" placeholder="请选择" size="small" clearable style="width:150px;">
+          <Option v-for="item in adPostionData" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
+      </FormItem>
+      <FormItem label="广告状态" prop="lockStatus">
+        <Select v-model="formSearch.lockStatus" placeholder="请选择" size="small" clearable style="width: 80px;">
+          <Option value="1">正常</Option>
+          <Option value="2">禁用</Option>
+          <Option value="3">过时</Option>
+        </Select>
+      </FormItem>
+      <FormItem label="投放级别" prop="areaType">
+        <Select v-model="formSearch.areaType" placeholder="请选择" size="small" @on-change="areaTypeChange_s">
+          <Option value="0">全部</Option>
+          <Option value="4">区级</Option>
+          <Option value="3">市级</Option>
+          <Option value="2">省级</Option>
+          <Option value="1">全国</Option>
+        </Select>
+      </FormItem>
+      <FormItem label="关联地区" prop="source">
+        <Cascader :data="derail_address_arr_s" v-model="derail_address_obj_s" :filterable="true" size="small"
+                  style="margin-top: 5px"></Cascader>
+      </FormItem>
+      <FormItem label="展示时间">
+        <FormItem prop="startTime">
+          <DatePicker type="datetime" placeholder="点击选择时间" v-model="formSearch.startTime" size="small"
+                       :clearable="false"></DatePicker>
+        </FormItem>
+        <FormItem>至</FormItem>
+        <FormItem prop="endTime">
+          <DatePicker type="datetime" placeholder="点击选择时间" v-model="formSearch.endTime" size="small"
+                       :clearable="false"></DatePicker>
+        </FormItem>
+      </FormItem>
+      <Button type="ghost" style="margin:5px 8px 24px 0;" @click="resetSearch('formSearch')" size="small">清空</Button>
+      <Button type="primary" style="margin: 5px 8px 24px 0;" @click="submitSearch('formSearch')" size="small">查找</Button>
+    </Form>
+    <mainTable :columns="columns" :data="pager.data"></mainTable>
+    <paging :total="pager.total"></paging>
+    <Modal v-model="dialogShow" :title="label[currDialog]" :mask-closable="false" width="750" @on-cancel="resetDialogForm('formDialog')">
+      <Form :model="formDialog" ref="formDialog" :rules="rules" :label-width="80">
+        <Row>
+          <Col span="24">
+            <FormItem label="标题" prop="title">
+              <Input v-model="formDialog.title" placeholder="请输入标题"></Input>
+            </FormItem>
+          </Col>
+        </Row>
+        <Row>
+          <Col span="24">
+            <FormItem label="广告内容" prop="context">
+              <Input v-model="formDialog.context" placeholder="请输入广告内容"></Input>
+            </FormItem>
+          </Col>
+        </Row>
+        <Row>
+          <Col span="12">
+            <Col span="12">
+              <FormItem label="主图" prop="imagePath">
+                <div class="image_upload_list">
+                  <img v-if="formDialog.imagePath!=null && formDialog.imagePath!=''" :src="formDialog.imagePath"/>
+                  <img v-else src="static/img/img-upload-default.png"/>
+                </div>
+              </FormItem>
+            </Col>
+          </Col>
+          <Col span="12">
+            <FormItem>
+              <Upload  name="upfile"
+                       action="ueditor/upload.do"
+                       :show-upload-list="false"
+                       :on-success="handleSuccess">
+                <Button type="ghost" icon="ios-cloud-upload-outline">上传</Button>
+              </Upload>
+            </FormItem>
+          </Col>
+        </Row>
+        <Row>
+          <Col span="12">
+            <FormItem label="链接地址" prop="href">
+              <Input type="url" v-model="formDialog.href" placeholder="请输入链接地址">
+              </Input>
+            </FormItem>
+          </Col>
+          <Col span="12">
+            <FormItem label="广告位置" prop="postion">
+              <Select v-model="formDialog.postion" placeholder="请选择"  clearable @on-change="postionChange">
+                <Option v-for="item in adPostionData" :value="item.value" :key="item.value">{{ item.label }}</Option>
+              </Select>
+            </FormItem>
+          </Col>
+        </Row>
+        <Row>
+          <Col span="12">
+            <FormItem label="排序" prop="sort" inline>
+              <InputNumber :max="10" :min="1" v-model="formDialog.sort"
+                            :disabled="formDialog.sort==1&&formDialog.isUp==2"></InputNumber>
+              <Button type="default" v-if="formDialog.sort==1"
+                        @click="formDialog.isUp = formDialog.isUp==1 ?2:1">{{formDialog.isUp==1 ? '置顶' :
+                '取消置顶'}}
+              </Button>
+            </FormItem>
+          </Col>
+          <Col span="12">
+            <FormItem label="">
+              <span v-if="true" style="font-size: 80%;color: #ff4d44;padding-left: 3px">
+                数值越小优先级越高
+              </span>
+            </FormItem>
+          </Col>
+        </Row>
+        <Row>
+          <Col span="12">
+            <FormItem label="开始时间" prop="startTime">
+              <DatePicker type="date" placeholder="点击选择时间" v-model="formDialog.startTime" :clearable="false"></DatePicker>
+            </FormItem>
+          </Col>
+          <Col span="12">
+            <FormItem label="结束时间" prop="endTime">
+              <DatePicker type="date" placeholder="点击选择时间" v-model="formDialog.endTime" :clearable="false"></DatePicker>
+            </FormItem>
+          </Col>
+        </Row>
+        <Row>
+          <Col span="12">
+            <FormItem label="投放级别" prop="areaType">
+              <Select v-model="formDialog.areaType" placeholder="请选择" @on-change="areaTypeChange">
+                <Option value="4">区级</Option>
+                <Option value="3">市级</Option>
+                <Option value="2">省级</Option>
+                <Option value="1">全国</Option>
+              </Select>
+            </FormItem>
+          </Col>
+          <Col span="12">
+            <FormItem label="关联地区" prop="derail_address_obj">
+              <Cascader :data="derail_address_arr" v-model="derail_address_obj" placeholder="选择关联地区"
+                        clearabled="false"></Cascader>
+            </FormItem>
+          </Col>
+        </Row>
+      </Form>
+      <div slot="footer">
+        <Button @click="resetDialogForm('formDialog')">{{label.clear}}</Button>
+        <Button type="primary" @click="submitDialogForm('formDialog')" :loading="dialogSubmitLoading">提交</Button>
+      </div>
+    </Modal>
   </div>
 </template>
 
 <script>
+  import mainTable from '@/components/mainTable'
+  import paging from '@/components/paging'
   export default {
-    name: 'ad'
+    name: 'ad',
+    components: {
+      mainTable,
+      paging
+    },
+    data () {
+      return {
+        label: {
+          'edit': '编辑',
+          'add': '添加',
+          'clear': '清空',
+          'submit': '提交',
+          'delete': '删除'
+        },
+        currDialog: 'edit',
+        dialogShow: false,
+        dialogSubmitLoading: false,
+        pager: {
+          data: [
+            {
+              id: 1231232,
+              userId: 12321213,
+              title: 'asdsad',
+              imagePath: 'sdfsdfdsf',
+              lockStatus: 1,
+              postion: 1,
+              href: 'sdfsdfsdf',
+              sort: 1,
+              clickNum: 12,
+              detailAddress: 'sdfsdf',
+              startTime: '',
+              endTime: '',
+              isUp: 1,
+              areaType: 1,
+              context: 'sddsff'
+            }
+          ],
+          total: 100
+        },
+        derail_address_arr: [],
+        derail_address_obj: [],
+        derail_address_arr_s: [],
+        derail_address_obj_s: [],
+        adPostionData: [
+          {
+            value: '1',
+            label: '首页banner'
+          }, {
+            value: '2',
+            label: '首页新闻列表'
+          }, {
+            value: '3',
+            label: '首页新闻详情'
+          }, {
+            value: '4',
+            label: '政务banner'
+          }, {
+            value: '5',
+            label: '招商banner'
+          }, {
+            value: '6',
+            label: '办事指南详情'
+          }, {
+            value: '7',
+            label: '便民工具'
+          }, {
+            value: '8',
+            label: '广场帖子列表'
+          }, {
+            value: '9',
+            label: '帖子详情'
+          }
+        ],
+        formSearch: {
+          startTime: '',
+          endTime: '',
+          province: 0,
+          city: 0,
+          type: null,
+          lockStatus: null,
+          title: null,
+          areaType: '0'
+        },
+        formDialog: {
+          id: '',
+          userId: '',
+          title: '',
+          imagePath: '',
+          lockStatus: '1',
+          postion: '1',
+          href: '',
+          sort: 1,
+          clickNum: '',
+          detailAddress: '',
+          startTime: '',
+          endTime: '',
+          isUp: '1',
+          areaType: '1',
+          context: ''
+        },
+        columns: [
+          {
+            'title': 'ID',
+            'key': 'id',
+            'width': 180,
+            'sortable': true
+          },
+
+          {
+            'title': '标题',
+            'key': 'title',
+            'width': 150,
+            'sortable': true
+          },
+
+          {
+            'title': '主图',
+            'key': 'imagePath',
+            'width': 120,
+            'sortable': true,
+            render: function (create, params) {
+              return create('img', {
+                attrs: {
+                  src: params.row.imagePath,
+                  width: 80,
+                  height: 80
+                },
+                style: {
+                  'border': '1px solid transparent',
+                  'border-radius': '4px',
+                  'margin': '10px 0'
+                }
+              })
+            }
+          },
+          {
+            'title': '链接地址',
+            'key': 'href',
+            'width': 250,
+            'sortable': true
+          },
+          {
+            'title': '点击次数',
+            'key': 'clickNum',
+            'width': 120,
+            'sortable': true
+          },
+          {
+            'title': '广告位置',
+            'key': 'postion',
+            'width': 120,
+            'sortable': true,
+            render: (create, params) => {
+              var map = [
+                {
+                  value: '1',
+                  label: '首页banner'
+                }, {
+                  value: '2',
+                  label: '首页新闻列表'
+                }, {
+                  value: '3',
+                  label: '首页新闻详情'
+                }, {
+                  value: '4',
+                  label: '政务banner'
+                }, {
+                  value: '5',
+                  label: '招商banner'
+                }, {
+                  value: '6',
+                  label: '办事指南详情'
+                }, {
+                  value: '7',
+                  label: '便民工具'
+                }, {
+                  value: '8',
+                  label: '广场帖子列表'
+                }, {
+                  value: '9',
+                  label: '帖子详情'
+                }
+              ]
+              return create('span', map[params.row.postion])
+            }
+          },
+          {
+            'title': '关联地区',
+            'key': 'detailAddress',
+            'width': 170,
+            'sortable': true,
+            render: function (create, params) {
+              var map = {
+                1: '', 2: ' (省)', 3: ' (市)', 4: ' (区)'
+              }
+              return create('span', params.row.detailAddress + map[params.row.areaType])
+            }
+
+          },
+          {
+            'title': '排序',
+            'key': 'sort',
+            'width': 100,
+            'sortable': true
+          },
+          {
+            'title': '状态',
+            'key': 'lockStatus',
+            'width': 100,
+            'sortable': true,
+            render: function (create, params) {
+              var map = {
+                1: '正常', 2: '禁用', 3: '过时'
+              }
+              if (params.row.isUp == 2) {
+                return create('span', map[params.row.lockStatus] + '(置顶)')
+              } else {
+                return create('span', map[params.row.lockStatus])
+              }
+            }
+          },
+          {
+            'title': '创建人',
+            'key': 'createBy',
+            'width': 120,
+            'sortable': true
+          },
+
+          {
+            'title': '开始时间',
+            'key': 'startTime',
+            'width': 160,
+            'sortable': true
+          },
+
+          {
+            'title': '结束时间',
+            'key': 'endTime',
+            'width': 160,
+            'sortable': true
+          },
+          {
+            'title': '创建时间',
+            'key': 'createTime',
+            'width': 160,
+            'sortable': true
+          },
+          {
+            title: '操作',
+            key: 'action',
+            width: 200,
+            align: 'center',
+            fixed: 'right',
+            render: (create, params) => {
+              return create('div', [
+                create('Button', {
+                  props: {
+                    type: 'primary',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.$store.commit('editRow', {
+                        'vm': this,
+                        'params': params,
+                        'initDialog': this.initDialog(params.row)
+                      })
+                    }
+                  }
+                }, '编辑'),
+                create('Button', {
+                  props: {
+                    type: 'warning',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      console.log('禁用')
+                    }
+                  }
+                }, '禁用'),
+                create('Button', {
+                  props: {
+                    type: 'error',
+                    size: 'small'
+                  },
+                  on: {
+                    click: () => {
+                      this.$store.dispatch('delRow', {
+                        'vm': this,
+                        'create': create,
+                        'params': params
+                      })
+                    }
+                  }
+                }, '删除')
+              ])
+            }
+          }
+        ],
+        rules: {}
+      }
+    },
+    methods: {
+      addRow () {
+        this.$store.commit('addRow', this)
+      },
+      resetSearch (name) {
+        var vm = this
+        vm.$refs[name].resetFields()
+      },
+      submitSearch (name) {
+        console.log('搜索')
+      },
+      resetDialogForm (name) {
+        let vm = this
+        vm.$refs[name].resetFields()
+      },
+      areaTypeChange () {},
+      areaTypeChange_s () {},
+      handleSuccess () {},
+      postionChange () {},
+      initDialog () {}
+    }
   }
 </script>
 
--- src/pages/news/index.vue
+++ src/pages/news/index.vue
@@ -1,13 +1,347 @@
 <template>
   <div class="news">
-    新闻管理
-    <router-link :to="{name: 'main'}">返回主页</router-link>
+    <!-- 高级搜索 -->
+    <Form :model="formSearch" ref="formSearch" inline :label-width="60">
+      <FormItem label="关键词" prop="keyWord">
+        <Input v-model="formSearch.keyWord" placeholder="标题" size="small" style="width: 120px"></Input>
+      </FormItem>
+      <FormItem label="新闻ID" prop="id">
+        <Input v-model="formSearch.id" placeholder="新闻ID" size="small" style="width: 120px"></Input>
+      </FormItem>
+      <FormItem label="来源" prop="newsSrc">
+        <Input v-model="formSearch.newsSrc" placeholder="新闻来源" size="small" style="width: 120px"></Input>
+      </FormItem>
+      <FormItem label="关联地区" prop="source">
+        <Cascader :data="derail_address_arr" v-model="derail_address_obj_s" filterable size="small" style="margin-top: 5px"></Cascader>
+      </FormItem>
+      <FormItem label="状态" prop="status">
+        <Select v-model="formSearch.status" placeholder="请选择" size="small" clearable>
+          <Option value="1">未发布</Option>
+          <Option value="2">已发布</Option>
+        </Select>
+      </FormItem>
+      <FormItem label="采集时间">
+        <FormItem prop="operatedateStart">
+          <DatePicker type="datetime" placeholder="点击选择时间" style="width: 160px;" v-model="formSearch.createdateStart" size="small"
+                       clearable></DatePicker>
+        </FormItem>
+        <FormItem>至</FormItem>
+        <FormItem prop="operatedateEnd">
+          <DatePicker type="datetime" placeholder="点击选择时间" style="width: 160px;" v-model="formSearch.createdateEnd" size="small"
+                       clearable></DatePicker>
+        </FormItem>
+      </FormItem>
+      <Button type="ghost" style="margin:5px 8px 24px 0;" @click="resetSearch('formSearch')" size="small">清空</Button>
+      <Button type="primary" style="margin: 5px 8px 24px 0;" @click="submitSearch('formSearch')" size="small">查找</Button>
+      <Button type="ghost" :disabled="batchOprArr.length==0" style="margin: 5px 8px 24px 0;" @click="batchDelete" size="small">批量删除</Button>
+      <Button type="primary" :disabled="batchOprArr.length==0" style="margin: 5px 8px 24px 0;" @click="batchPublish" size="small">批量发布</Button>
+    </Form>
+    <mainTable :columns="columns" :data="pager.data"></mainTable>
+    <paging :total="pager.total"></paging>
+    <Modal v-model="dialogShow" :title="label[currDialog]" :mask-closable="false" width="750" @on-cancel="resetDialogForm('formDialog')">
+      <Form :model="formDialog" ref="formDialog" :rules="rules" :label-width="80">
+        <Row>
+          <Col span="14">
+            <FormItem label="新闻标题" prop="title">
+              <Input v-model="formDialog.title" placeholder="请输入标题"></Input>
+            </FormItem>
+            <FormItem label="新闻来源" prop="newsSrc">
+              <Input v-model="formDialog.newsSrc" placeholder="请输入新闻来源"></Input>
+            </FormItem>
+            <FormItem label="关联地区" prop="areaId">
+              <Input v-model="formDialog.detailAddress" :disabled="true"></Input>
+            </FormItem>
+          </Col>
+          <Col span="10">
+            <FormItem label="新闻主图" prop="image">
+              <Row>
+                <Col span="12">
+                  <div class="image_upload_list">
+                    <img v-bind:src="formDialog.image"/>
+                  </div>
+                </Col>
+                <Col span="12">
+                  <Upload name="upfile" action="ueditor/upload.do" :show-upload-list="false" :on-success="handleSuccess">
+                    <i-button type="ghost" icon="ios-cloud-upload-outline">上传</i-button>
+                  </Upload>
+                </Col>
+              </Row>
+            </FormItem>
+          </Col>
+        </Row>
+        <Row>
+          <Col span="24">
+            <FormItem label="新闻内容" prop="content">
+              <Input v-model="formDialog.content" placeholder="新闻内容富文本"></Input>
+            </FormItem>
+          </Col>
+        </Row>
+      </Form>
+      <div slot="footer">
+        <Button type="primary" @click="submitDialogForm('formDialog')" :loading="dialogSubmitLoading">提交</Button>
+      </div>
+    </Modal>
   </div>
 </template>
 
 <script>
+  import mainTable from '@/components/mainTable'
+  import paging from '@/components/paging'
   export default {
-    name: 'news'
+    name: 'news',
+    components: {
+      mainTable,
+      paging
+    },
+    data: function () {
+      return {
+        label: {
+          'edit': '编辑',
+          'add': '添加',
+          'clear': '清空',
+          'submit': '提交',
+          'delete': '删除'
+        },
+        currDialog: 'edit',
+        dialogShow: false,
+        dialogSubmitLoading: false,
+        pager: {
+          data: [
+            {
+              id: '2131240',
+              areaId: '112233',
+              url: 'www.baidu.com',
+              title: 'qweqwe',
+              newsSrc: 'asdasd',
+              content: 'asdasdsa',
+              date: 'asdasd',
+              image: 'asdasd',
+              commentNum: 'asd',
+              upvoteNum: 'xcv',
+              shareNum: 'xcvb',
+              status: 'bnm,',
+              detailAddress: 'hjkhjk'
+            }
+          ],
+          total: 100
+        },
+        derail_address_arr: [],
+        derail_address_arr_s: [],
+        derail_address_obj_s: [],
+        batchOprArr: [],
+        formSearch: {
+          startTime: '',
+          endTime: '',
+          province: 0,
+          city: 0,
+          type: null,
+          lockStatus: null,
+          title: null,
+          areaType: '0'
+        },
+        formDialog: {
+          id: '0',
+          areaId: '',
+          url: '',
+          title: '',
+          newsSrc: '',
+          content: '',
+          date: '',
+          image: '',
+          commentNum: '',
+          upvoteNum: '',
+          shareNum: '',
+          status: '',
+          detailAddress: ''
+        },
+        columns: [
+          {
+            type: 'selection',
+            width: 80,
+            fixed: 'left',
+            align: 'center'
+          },
+          {
+            'title': 'ID',
+            'key': 'id',
+            'width': 150,
+            'sortable': true
+          },
+          {
+            'title': '区域ID',
+            'key': 'areaId',
+            'width': 120,
+            'sortable': true
+          },
+          {
+            'title': '新闻图片',
+            'key': 'image',
+            'width': 120,
+            'sortable': true,
+            render: function (create, params) {
+              return create('img', {
+                attrs: {
+                  src: params.row.image,
+                  width: 80,
+                  height: 80
+                },
+                style: {
+                  'border': '1px solid transparent',
+                  'border-radius': '4px'
+                }
+              })
+            }
+          },
+          {
+            'title': '标题',
+            'key': 'title',
+            'width': 300,
+            'sortable': true
+          },
+          {
+            'title': '新闻来源',
+            'key': 'newsSrc',
+            'width': 150,
+            'sortable': true
+          },
+          {
+            'title': '关联地区',
+            'key': 'detailAddress',
+            'width': 160,
+            'sortable': true
+          },
+          {
+            'title': '评论数',
+            'key': 'commentNum',
+            'width': 100,
+            'sortable': true
+          },
+          {
+            'title': '点赞数',
+            'key': 'upvoteNum',
+            'width': 100,
+            'sortable': true
+          },
+          {
+            'title': '分享数',
+            'key': 'shareNum',
+            'width': 100,
+            'sortable': true
+          },
+          {
+            'title': '状态',
+            'key': 'status',
+            'width': 100,
+            'sortable': true,
+            render: function (create, params) {
+              var map = {
+                0: '未发布', 1: '未发布', 2: '已发布'
+              }
+              return create('span', map[params.row.status])
+            }
+          },
+          {
+            'title': '采集时间',
+            'key': 'createTime',
+            'width': 160,
+            'sortable': true
+          },
+          {
+            title: '操作',
+            key: 'action',
+            width: 200,
+            align: 'center',
+            fixed: 'right',
+            render: (create, params) => {
+              return create('div', [
+                create('Button', {
+                  props: {
+                    type: 'primary',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      this.$store.commit('editRow', {
+                        'vm': this,
+                        'params': params,
+                        'initDialog': this.initDialog(params.row)
+                      })
+                    }
+                  }
+                }, '编辑'),
+                create('Button', {
+                  props: {
+                    type: 'primary',
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      console.log('查看')
+                    }
+                  }
+                }, '查看'),
+                create('Button', {
+                  props: {
+                    size: 'small'
+                  },
+                  style: {
+                    marginRight: '5px'
+                  },
+                  on: {
+                    click: () => {
+                      console.log('发布')
+                    }
+                  }
+                }, '发布'),
+                create('Button', {
+                  props: {
+                    type: 'error',
+                    size: 'small'
+                  },
+                  on: {
+                    click: () => {
+                      this.$store.dispatch('delRow', {
+                        'vm': this,
+                        'create': create,
+                        'params': params
+                      })
+                    }
+                  }
+                }, '删除')
+              ])
+            }
+          }
+        ],
+        rules: {}
+      }
+    },
+    methods: {
+      resetSearch (name) {
+        var vm = this
+        vm.$refs[name].resetFields()
+      },
+      submitSearch (name) {
+        console.log('搜索')
+      },
+      batchDelete () {
+        console.log('批量删除')
+      },
+      batchPublish () {
+        console.log('批量发布')
+      },
+      handleSuccess () {},
+      resetDialogForm (name) {
+        let vm = this
+        vm.$refs[name].resetFields()
+      },
+      initDialog (data) {}
+    }
   }
 </script>
 
